ad7db8595d053f8ed9abb3de211175f5bacd3bd7
Antonio Ponce <antonioponce3623@gmail.com>
2025-10-01 03:51:26 -0600

    feat: Añade pantalla completa sin bordes y mejoras de ventana

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import sys
 # Inicializar Pygame
 pygame.init()
 
-# Configuración de la pantalla
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Lotería")
+#Optenemos la informacion del Display actual
+try:
+    info_display = pygame.display.Info()
+    screen_width = info_display.current_w
+    screen_height = info_display.current_h
+except pygame.error as e:
+    # Si falla (muy raro), volvemos a un tamaño seguro por defecto
+    print(f"No se pudo obtener la información del display: {e}")
+    screen_width, screen_height = 800, 600
+
+# Ahora creamos la ventana usando la resolución obtenida y la bandera NOFRAME
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME)
+pygame.display.set_caption("Lotería Orus")
 
 # Bucle principal del juego
 running = True
@@ -14,6 +24,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # Si se presiona una tecla
+        if event.type == pygame.KEYDOWN:
+            # Si la tecla es ESCAPE
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     screen.fill((0, 128, 0)) # Fondo verde
     pygame.display.flip()
